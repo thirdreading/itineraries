5807ef11be500a64f062a9b2530befd6d6d928ee
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-20 06:23:27 +0100

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -1,15 +1,22 @@
-import os
+"""
+interface.py
+"""
 import logging
-
-import pandas as pd
+import os
 
 import src.functions.directories
 import src.functions.streams
 
 
 class Interface:
+    """
+    Interface
+    """
 
     def __init__(self):
+        """
+        Constructor
+        """
 
         self.__source = os.path.join(os.getcwd(), 'data')
         self.__storage = os.path.join(os.getcwd(), 'warehouse', 'data')
@@ -24,34 +31,14 @@ class Interface:
         self.__logger = logging.getLogger(__name__)
 
     def __set_up(self):
+        """
+
+        :return: None
+        """
 
         directories = src.functions.directories.Directories()
         directories.cleanup(path=self.__storage)
         directories.create(path=self.__storage)
 
-    def __publication_type(self) -> pd.DataFrame:
-
-        return self.__streams.read(uri=os.path.join(self.__source, 'publication_type.csv'))
-
-    def __theme(self) -> pd.DataFrame:
-
-        return self.__streams.read(uri=os.path.join(self.__source, 'theme.csv'))
-
-    def __schedule(self) -> pd.DataFrame:
-
-        data = src.functions.streams.Streams().read(uri=os.path.join(self.__source, 'schedule.csv'), header=0)
-        data.rename(mapper=str.lower, axis=1, inplace=True)
-        data.columns = data.columns.str.replace(' ', '_')
-
-        return data
-
     def exc(self):
-
-        initial = self.__schedule()
-        data = initial.copy().merge(self.__publication_type(), how='inner', on='publication_type')
-        data = data.copy().merge(self.__theme(), how='inner', on='theme_name')
-
-        assert initial.shape[0] == data.shape[0], 'Missing records due to unknown dimensions?'
-
-        self.__logger.info('%s', initial.info())
-        self.__logger.info('%s', data.info())
+        pass
