e7c7d4dfa9165cbb35dd385224f9cb5991b5524b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-20 05:33:11 +0100

    Add src/data/main.py: In Progress

---
+++ src/data/main.py
@@ -0,0 +1,25 @@
+import logging
+import os
+import sys
+
+
+def main():
+
+    logger.info(__name__)
+
+
+if __name__ == '__main__':
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Threads
+    os.environ['NUMEXPR_MAX_THREADS'] = '4'
+
+    # logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logger = logging.getLogger(__name__)
+
+    main()
