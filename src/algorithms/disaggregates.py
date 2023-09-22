d9fb8a51ec665f78321f7cbc8e74da7749ab7867
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-22 08:05:44 +0100

    Update src/algorithms/disaggregates.py

--- src/algorithms/disaggregates.py
+++ src/algorithms/disaggregates.py
@@ -26,6 +26,9 @@ class Disaggregates:
         self.__theme = theme
         self.__storage = storage
 
+        # fields
+        self.__fields = {'epoch': 'x', 'publication_series': 'name', 'synopsis': 'description'}
+
         # logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -40,7 +43,8 @@ class Disaggregates:
         :return:
         """
 
-        frame = self.__data.copy().loc[self.__data['publication_id'] == code, :]
+        frame: pd.DataFrame = self.__data.copy().loc[self.__data['publication_id'] == code, self.__fields.keys()]
+        frame.rename(columns=self.__fields, inplace=True)
 
     def exc(self):
         """
