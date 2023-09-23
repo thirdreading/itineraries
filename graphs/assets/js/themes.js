16d50f0821a96c8df67f603d8a3c0a7d15f3c89e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-09-23 14:56:55 +0100

    Update graphs/assets/js/themes.js

---
+++ graphs/assets/js/themes.js
@@ -0,0 +1,38 @@
+// Declarations
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = 'https://raw.githubusercontent.com/thirdreading/itineraries/develop/warehouse/data/themes.json';
+
+
+// Menu data
+$.getJSON(url, function (source) {
+
+    $.each(source, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.name).text(entry.desc));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption, optionSelected);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected, optionSelected);
+
+});
+
